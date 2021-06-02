aae124241de25d93007471732a00a10c39e058d3
hoxxii <winterluis77@gmail.com>
2021-06-02 20:34:10 +0200

    Tolly's Helmet added in.
    This is Tolly's Helmet

---
+++ addons/TAS_Headgear/config.cpp
@@ -0,0 +1,26 @@
+class CfgPatches
+{
+	class TAS_Headgear
+	{
+		requiredAddons[]=
+		{
+			"A3_Weapons_F"
+		};
+		requiredVersion=0.1;
+		units[]={};
+		weapons[]={};
+		{
+	};
+};
+
+class cfgWeapons
+{
+	class H_HelmetB;
+	class TAS_TestHelmet: H_CrewHelmetHeli_B
+	{ 
+		author = "Tolly";
+		displayName = "Tolly's Helmet";
+		picture = "A3\Weapons_F\Data\placeholder_co.paa";
+		hiddenSelectionsTextures[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Headgear\Textures\TAS_Tolly_CrewHelm1.paa"};
+	};
+}; 
