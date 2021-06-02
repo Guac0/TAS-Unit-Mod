13a8bb0011110e3c7c19cb39c977e93df5f1ec31
Guac0 <YeetYeetGuac@gmail.com>
2021-06-02 10:47:02 -0400

    WIP changes to light distance

--- addons/TAS_BrightLite/config.cpp
+++ addons/TAS_BrightLite/config.cpp
@@ -8,7 +8,7 @@ class CfgPatches
 		weapons[] = {"TAS_acc_brightlite","TAS_Item_acc_brightlite","arifle_TRG20_ACO_BrightLite_F"};
 		requiredAddons[] = {"A3_Weapons_F","cba_main"};
 		//version = "0.3";
-		author[]= {"TAS Mod Team"};
+		author = "TAS Mod Team";
 		authorUrl = "https://steamcommunity.com/id/YT_YYG_Gaming";
 	};
 };
@@ -56,6 +56,65 @@ class CfgWeapons
 				position="flash dir";
 				direction="flash";
 				size=1;
+				innerAngle=5; //lowered from 25
+				outerAngle=20; //lowered from 40
+				coneFadeCoef=2; //
+				intensity=5;
+				useFlare=1;
+				dayLight=1;
+				FlareSize=0.7;
+				flareMaxDistance=400;
+				onlyInNvg=0;
+				scale[]={0};
+				class Attenuation
+				{
+					start=80; //lowered 
+					constant=1; //lowered
+					linear=1; //lowered
+					quadratic=1; //15
+					hardLimitStart=120; //130
+					hardLimitEnd=150; //160
+				};
+			};
+			/* dazzler
+			class ItemInfo: InventoryFlashLightItem_Base_F
+		{
+			mass = 4;
+			class FlashLight
+			{
+				color[]={600,750,900};
+				ambient[]={10,7.5,5};
+				position="flash dir";
+				direction="flash";
+				size=1;
+				innerAngle=25;
+				outerAngle=40;
+				coneFadeCoef=1; //lowered
+				intensity=100;
+				useFlare=1;
+				dayLight=1;
+				FlareSize=0.69999999;
+				flareMaxDistance=250;
+				onlyInNvg=0;
+				scale[]={0};
+				class Attenuation
+				{
+					start=55; //lowered 
+					constant=1; //lowered
+					linear=2; //lowered
+					quadratic=15;
+					hardLimitStart=130;
+					hardLimitEnd=160;
+				};
+			}; */
+			/* real engine flashlight
+			class FlashLight
+			{
+				color[]={600,750,900};
+				ambient[]={10,7.5,5};
+				position="flash dir";
+				direction="flash";
+				size=1;
 				innerAngle=25;
 				outerAngle=40;
 				coneFadeCoef=2;
@@ -68,14 +127,41 @@ class CfgWeapons
 				scale[]={0};
 				class Attenuation
 				{
-					start=55;
-					constant=1;
-					linear=2;
+					start=15;
+					constant=100;
+					linear=70;
 					quadratic=15;
 					hardLimitStart=130;
 					hardLimitEnd=160;
 				};
-			};
+			}; */
+			/* BIS flashlight
+			class FlashLight
+				{
+					color[] = {180, 160, 130};
+					ambient[] = {0.9, 0.81, 0.7};
+					intensity = 100;
+					size = 1;
+					innerAngle = 5;
+					outerAngle = 100;
+					coneFadeCoef = 8;
+					position = "flash dir";
+					direction = "flash";
+					useFlare = 1;
+					flareSize = 1.4;
+					flareMaxDistance = 100;
+					dayLight = 0;
+					class Attenuation
+					{
+						start = 0;
+						constant = 0.5;
+						linear = 0.1;
+						quadratic = 0.2;
+						hardLimitStart = 27;
+						hardLimitEnd = 34;
+					};
+					scale[] = {0};
+				};*/
 		};
 		inertia = 0.1;
 	};
