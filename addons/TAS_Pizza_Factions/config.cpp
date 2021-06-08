c30a16198004b18058d357a8476fdaad86b5f831
Guac0 <YeetYeetGuac@gmail.com>
2021-06-08 12:49:54 -0400

    Pizza Rework and Expansion of Dominos Units

--- addons/TAS_Pizza_Factions/config.cpp
+++ addons/TAS_Pizza_Factions/config.cpp
@@ -6,7 +6,10 @@ class CfgPatches
 		name = "TAS Faction Addon";
 		units[] = 
 		{
+			"Dominos_BaseUnit",
 			"Dominos_Employee",
+			"Dominos_Manager",
+			"Dominos_Operative",
 			"PapaJhons_Employee",
 			"Custom_Offroad_F2",
 			"Custom_Offroad_F3"
@@ -20,6 +23,21 @@ class CfgPatches
 		authorUrl = "https://discord.gg/invite/mcXfzqR5Kn";
 	};
 };
+
+//************************************************************************************************************************************************************************************************
+//*****             Definitions                *********************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+#define MAG_2(a) a, a
+#define MAG_3(a) a, a, a
+#define MAG_4(a) a, a, a, a
+#define MAG_5(a) a, a, a, a, a
+#define MAG_6(a) a, a, a, a, a, a
+#define MAG_7(a) a, a, a, a, a, a, a
+#define MAG_8(a) a, a, a, a, a, a, a, a
+#define MAG_9(a) a, a, a, a, a, a, a, a, a
+#define MAG_10(a) a, a, a, a, a, a, a, a, a, a
+#define MAG_11(a) a, a, a, a, a, a, a, a, a, a, a
+#define MAG_12(a) a, a, a, a, a, a, a, a, a, a, a, a
 
 //************************************************************************************************************************************************************************************************
 //*****        Factions                  *********************************************************************************************************************************************************
@@ -73,9 +91,17 @@ class CfgVehicles
 	class O_G_Offroad_01_F;
 	class B_G_Offroad_01_F;
 	
-    class Dominos_Employee: B_soldier_F {
+	//************************************************************************************************************************************************************************************************
+    //*****             Dominos Units                *********************************************************************************************************************************************************
+    //************************************************************************************************************************************************************************************************
+    //helmets and carriers
+	//H_Cap_blu, H_PASGT_basic_black_F, H_HelmetB_TI_arid_F, H_HelmetAggressor_F
+	//V_LegStrapBag_black_F, V_TacVest_blk, V_PlateCarrier1_blk, V_PlateCarrierSpec_blk
+	//B_AssaultPack_blk
+	class Dominos_BaseUnit: B_soldier_F
+	{
 		side = 1; //blufor
-        author = "Ricardo"; 
+        author = "CatCaptain"; 
         //_generalMacro = "B_soldier_F"; //internal BIS debug
         scope = 2;							// 2 = class is available in the editor; 1 = class is unavailable in the editor, but can be accessed via a macro; 0 = class is unavailable (and used for inheritance only).
 		scopeCurator = 2;					// 2 = class is available in Zeus; 0 = class is unavailable in Zeus.
@@ -84,7 +110,7 @@ class CfgVehicles
 		camouflage = 1.5;					// How likely this character is spotted (smaller number = more stealthy).
 		sensitivity = 2.5;					// How likely this character spots enemies when controlled by AI.
 		threat[] = {1, 1, 0.8};*/				// Multiplier of the cost of the character in the eyes of soft, armoured and air enemies.
-        displayName = "Dominos Employee"; 
+        displayName = "Dominos Employee (Unarmed)"; 
         identityTypes[] = {"Head_NATO", "G_NATO_default"}; 
         genericNames = "NATOMen"; //name list this unit draws from
         faction = "Domino_Zimbabwe"; //classname of faction this belongs to
@@ -97,15 +123,100 @@ class CfgVehicles
         canDeactivateMines = true;				// Can this character deactivate mines?
 		engineer = true;						// Can this character repair vehicles?
 		attendant = 1;							// Can this character heal soldiers?
-		weapons[] = {"arifle_TRG20_ACO_Flash_F","Throw","Put"}; // Which weapons the character has.
-        respawnWeapons[] = {"arifle_TRG20_ACO_Flash_F","Throw","Put"}; // Which weapons the character respawns with.
-        magazines[] = {"HandGrenade","HandGrenade","SmokeShell","SmokeShellGreen","Chemlight_green","Chemlight_green"}; // Which items the character has.
-        respawnMagazines[] = {"HandGrenade","HandGrenade","SmokeShell","SmokeShellGreen","Chemlight_green","Chemlight_green"}; // Which items the character respawns with.
-        linkedItems[] = {"ItemMap","ItemCompass","ItemWatch","ItemRadio"}; // Which items the character has.
-        respawnLinkedItems[] = {"ItemMap","ItemCompass","ItemWatch","ItemRadio"}; // Which items the character respawns with.
+		weapons[] = {Throw,Put}; // Which weapons the character has.
+        respawnWeapons[] = {Throw,Put}; // Which weapons the character respawns with.
+        Items[] = {MAG_3(FirstAidKit)};				// Which items the character has.
+		RespawnItems[] = {MAG_3(FirstAidKit)};			// Which items the character respawns with.
+		magazines[] = {};				// What ammunition the character has.
+		respawnMagazines[] = {};		// What ammunition the character respawns with.
+		linkedItems[] = {H_Cap_blu, ItemMap, ItemCompass, ItemWatch, ItemRadio};				// Which items the character has.
+		respawnLinkedItems[] = {H_Cap_blu, ItemMap, ItemCompass, ItemWatch, ItemRadio};		// Which items the character respawns with.
     };
+	class Dominos_Employee: Dominos_BaseUnit
+	{
+        author = "Guac"; 
+		/*cost = 200000;						// How likely the enemies attack this character among some others.
+		camouflage = 1.5;					// How likely this character is spotted (smaller number = more stealthy).
+		sensitivity = 2.5;					// How likely this character spots enemies when controlled by AI.
+		threat[] = {1, 1, 0.8};*/				// Multiplier of the cost of the character in the eyes of soft, armoured and air enemies.
+        displayName = "Dominos Employee"; 
+        canDeactivateMines = true;				// Can this character deactivate mines?
+		engineer = true;						// Can this character repair vehicles?
+		attendant = 1;							// Can this character heal soldiers?
+		weapons[] = {hgun_Pistol_heavy_01_MRD_F,Throw,Put}; // Which weapons the character has.
+        respawnWeapons[] = {hgun_Pistol_heavy_01_MRD_F,Throw,Put}; // Which weapons the character respawns with.
+        Items[] = {MAG_3(FirstAidKit)};				// Which items the character has.
+		RespawnItems[] = {MAG_3(FirstAidKit)};			// Which items the character respawns with.
+		magazines[] = {MAG_5(11Rnd_45ACP_Mag), MAG_2(SmokeShell), MAG_2(Chemlight_green)};				// What ammunition the character has.
+		respawnMagazines[] = {MAG_5(11Rnd_45ACP_Mag), MAG_2(SmokeShell), MAG_2(Chemlight_green)};		// What ammunition the character respawns with.
+		linkedItems[] = {H_Cap_blu, V_LegStrapBag_black_F, ItemMap, ItemCompass, ItemWatch, ItemRadio};				// Which items the character has.
+		respawnLinkedItems[] = {H_Cap_blu, V_LegStrapBag_black_F, ItemMap, ItemCompass, ItemWatch, ItemRadio};		// Which items the character respawns with.
+    };
+	class Dominos_EmployeeCombat: Dominos_BaseUnit
+	{
+        author = "Guac"; 
+		/*cost = 200000;						// How likely the enemies attack this character among some others.
+		camouflage = 1.5;					// How likely this character is spotted (smaller number = more stealthy).
+		sensitivity = 2.5;					// How likely this character spots enemies when controlled by AI.
+		threat[] = {1, 1, 0.8};*/				// Multiplier of the cost of the character in the eyes of soft, armoured and air enemies.
+        displayName = "Dominos Employee (Combat)"; 
+        canDeactivateMines = true;				// Can this character deactivate mines?
+		engineer = true;						// Can this character repair vehicles?
+		attendant = 1;							// Can this character heal soldiers?
+		weapons[] = {arifle_TRG20_ACO_Flash_F,hgun_Pistol_heavy_01_MRD_F,Throw,Put}; // Which weapons the character has.
+        respawnWeapons[] = {arifle_TRG20_ACO_Flash_F,hgun_Pistol_heavy_01_MRD_F,Throw,Put}; // Which weapons the character respawns with.
+        Items[] = {MAG_3(FirstAidKit)};				// Which items the character has.
+		RespawnItems[] = {MAG_3(FirstAidKit)};			// Which items the character respawns with.
+		magazines[] = {MAG_6(30Rnd_556x45_Stanag_red),MAG_2(11Rnd_45ACP_Mag), MAG_2(SmokeShell), MAG_2(Chemlight_green), MAG_2(HandGrenade)};				// What ammunition the character has.
+		respawnMagazines[] = {MAG_6(30Rnd_556x45_Stanag_red),MAG_2(11Rnd_45ACP_Mag), MAG_2(SmokeShell), MAG_2(Chemlight_green), MAG_2(HandGrenade)};		// What ammunition the character respawns with.
+		linkedItems[] = {V_TacVest_blk, H_PASGT_basic_black_F, ItemMap, ItemCompass, ItemWatch, ItemRadio};				// Which items the character has.
+		respawnLinkedItems[] = {V_TacVest_blk, H_PASGT_basic_black_F, ItemMap, ItemCompass, ItemWatch, ItemRadio};		// Which items the character respawns with.
+    };
+	class Dominos_Manager: Dominos_BaseUnit
+	{
+        author = "Guac"; 
+		/*cost = 200000;						// How likely the enemies attack this character among some others.
+		camouflage = 1.5;					// How likely this character is spotted (smaller number = more stealthy).
+		sensitivity = 2.5;					// How likely this character spots enemies when controlled by AI.
+		threat[] = {1, 1, 0.8};*/				// Multiplier of the cost of the character in the eyes of soft, armoured and air enemies.
+        displayName = "Dominos Manager"; 
+        canDeactivateMines = true;				// Can this character deactivate mines?
+		engineer = true;						// Can this character repair vehicles?
+		attendant = 1;							// Can this character heal soldiers?
+		weapons[] = {arifle_TRG21_GL_F,hgun_Pistol_heavy_01_MRD_F,Throw,Put}; // Which weapons the character has.
+        respawnWeapons[] = {arifle_TRG21_GL_F,hgun_Pistol_heavy_01_MRD_F,Throw,Put}; // Which weapons the character respawns with.
+        Items[] = {MAG_3(FirstAidKit)};				// Which items the character has.
+		RespawnItems[] = {MAG_3(FirstAidKit)};			// Which items the character respawns with.
+		magazines[] = {MAG_6(30Rnd_556x45_Stanag_red),MAG_2(11Rnd_45ACP_Mag), MAG_2(SmokeShell), MAG_2(SmokeShellRed), MAG_2(SmokeShellBlue), MAG_7(1Rnd_HE_Grenade_shell), MAG_4(1Rnd_Smoke_Grenade_shell), MAG_4(UGL_FlareWhite_F), MAG_2(Chemlight_green), MAG_2(HandGrenade)};				// What ammunition the character has.
+		respawnMagazines[] = {MAG_6(30Rnd_556x45_Stanag_red),MAG_2(11Rnd_45ACP_Mag), MAG_2(SmokeShell), MAG_2(SmokeShellRed), MAG_2(SmokeShellBlue), MAG_7(1Rnd_HE_Grenade_shell), MAG_4(1Rnd_Smoke_Grenade_shell), MAG_4(UGL_FlareWhite_F), MAG_2(Chemlight_green), MAG_2(HandGrenade)};		// What ammunition the character respawns with.
+		linkedItems[] = {V_PlateCarrier1_blk, H_HelmetAggressor_F, ItemMap, ItemCompass, ItemWatch, ItemRadio};				// Which items the character has.
+		respawnLinkedItems[] = {V_PlateCarrier1_blk, H_HelmetAggressor_F, ItemMap, ItemCompass, ItemWatch, ItemRadio};		// Which items the character respawns with.
+    };
+	class Dominos_Operative: Dominos_BaseUnit
+	{
+        author = "Guac"; 
+		/*cost = 200000;						// How likely the enemies attack this character among some others.
+		camouflage = 1.5;					// How likely this character is spotted (smaller number = more stealthy).
+		sensitivity = 2.5;					// How likely this character spots enemies when controlled by AI.
+		threat[] = {1, 1, 0.8};*/				// Multiplier of the cost of the character in the eyes of soft, armoured and air enemies.
+        displayName = "Dominos Operative"; 
+        canDeactivateMines = true;				// Can this character deactivate mines?
+		engineer = true;						// Can this character repair vehicles?
+		attendant = 1;							// Can this character heal soldiers?
+		weapons[] = {arifle_SPAR_02_blk_ERCO_Pointer_F,hgun_Pistol_heavy_01_MRD_F,Throw,Put}; // Which weapons the character has.
+        respawnWeapons[] = {arifle_SPAR_02_blk_ERCO_Pointer_F,hgun_Pistol_heavy_01_MRD_F,Throw,Put}; // Which weapons the character respawns with.
+        Items[] = {MAG_3(FirstAidKit)};				// Which items the character has.
+		RespawnItems[] = {MAG_3(FirstAidKit)};			// Which items the character respawns with.
+		magazines[] = {MAG_4(150Rnd_556x45_Drum_Mag_F),MAG_2(11Rnd_45ACP_Mag), MAG_2(SmokeShell), MAG_2(SmokeShellRed), MAG_2(SmokeShellBlue), MAG_2(Chemlight_green), MAG_4(HandGrenade)};				// What ammunition the character has.
+		respawnMagazines[] = {MAG_4(150Rnd_556x45_Drum_Mag_F),MAG_2(11Rnd_45ACP_Mag), MAG_2(SmokeShell), MAG_2(Chemlight_green), MAG_2(HandGrenade)};		// What ammunition the character respawns with.
+		linkedItems[] = {V_PlateCarrierSpec_blk, H_HelmetB_TI_arid_F, ItemMap, ItemCompass, ItemWatch, ItemRadio};				// Which items the character has.
+		respawnLinkedItems[] = {V_PlateCarrierSpec_blk, H_HelmetB_TI_arid_F, ItemMap, ItemCompass, ItemWatch, ItemRadio};	
+    };
+	//************************************************************************************************************************************************************************************************
+    //*****             Papa Jhons Units                *********************************************************************************************************************************************************
+    //************************************************************************************************************************************************************************************************
 	class PapaJhons_Employee: O_Soldier_base_F {
-        author = "Ricardo"; 
+		author = "CatCaptain";
         _generalMacro = "B_soldier_F"; 
         scope = 2; 
         displayName = "Papa John Employee"; 
@@ -135,7 +246,8 @@ class CfgVehicles
     };
 	class Custom_Offroad_F2: O_G_Offroad_01_F
     {
-        crew = "PapaJhons_Employee";
+        author = "CatCaptain";
+		crew = "PapaJhons_Employee";
         side = 0;
         scope = 2;
         faction = "Papa_John";
@@ -149,7 +261,8 @@ class CfgVehicles
     };
 	class Custom_Offroad_F3: B_G_Offroad_01_F
     {
-        crew = "Dominos_Employee";
+        author = "CatCaptain";
+		crew = "Dominos_Employee";
         side = 1;
         scope = 2;
         faction = "Domino_Zimbabwe";
@@ -178,7 +291,7 @@ class cfgWeapons
 
     class Dominos_Uniform: Uniform_Base 
     { 
-        author = "CatCaptain";
+        author = "Ricardo";
 		scope = 2; /// scope needs to be 2 to have a visible class
 		allowedSlots[] = {BACKPACK_SLOT}; /// where does the uniform fit to when not equipped
         displayName = "Dominos Uniform"; /// how would the stuff be displayed in inventory and on ground
@@ -199,7 +312,7 @@ class cfgWeapons
     }; 
 	class Papa_John_Uniform: Uniform_Base 
     { 
-        author = "CatCaptain";
+        author = "Ricardo";
 		scope = 2; 
 		allowedSlots[] = {BACKPACK_SLOT};
         displayName = "Papa Jhon Uniform"; 
