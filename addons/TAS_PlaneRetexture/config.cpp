75cb1d99026a0e424b3781c9b813e85533a6f55a
Guac0 <YeetYeetGuac@gmail.com>
2021-06-11 11:34:52 -0400

    Plane Updated Both Textures

--- addons/TAS_PlaneRetexture/config.cpp
+++ addons/TAS_PlaneRetexture/config.cpp
@@ -10,7 +10,7 @@ class CfgPatches
 		requiredVersion=0.1;
 		units[]=
 		{
-			"TAS_A164_Hentai",
+			"TAS_A164_Ahegao",
 			"TAS_A164_Kirby"
 		};
 		weapons[]={};
@@ -23,16 +23,16 @@ class CfgVehicles
 {
 	class B_Plane_CAS_01_F; //unused
 	class B_Plane_CAS_01_dynamicLoadout_F;
-	class TAS_A164_Hentai: B_Plane_CAS_01_dynamicLoadout_F
+	class TAS_A164_Ahegao: B_Plane_CAS_01_dynamicLoadout_F
 	{
 		author = "Guac";
-		displayName = "Hentai A-164";
+		displayName = "Ahegao A-164";
 		scope = 2;
 		scopecurator = 2;
 		//faction = "Domino_Zimbabwe";
 		hiddenSelections[] = {"Camo_1", "Camo_2"};
 		//hiddenSelectionsTextures[] = {"A3\Air_F_EPC\Plane_CAS_01\Data\plane_cas_01_ext01_co.paa", "A3\Air_F_EPC\Plane_CAS_01\Data\plane_cas_01_ext02_co.paa"};
-		hiddenSelectionsTextures[] = {"Autismo_Seals_Unit_Mod\addons\TAS_PlaneRetexture\Data\a164_hentai.paa", "A3\Air_F_EPC\Plane_CAS_01\Data\plane_cas_01_ext02_co.paa"};
+		hiddenSelectionsTextures[] = {"Autismo_Seals_Unit_Mod\addons\TAS_PlaneRetexture\Data\a164_ahegao_ext01_CO.paa", "Autismo_Seals_Unit_Mod\addons\TAS_PlaneRetexture\Data\a164_ahegao_ext02_CO.paa"};
 	};
 	class TAS_A164_Kirby: B_Plane_CAS_01_dynamicLoadout_F
 	{
