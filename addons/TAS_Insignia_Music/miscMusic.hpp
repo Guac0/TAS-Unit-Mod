03c03ce4c50f7007efcc09f7a30d5231db2c6c41
Guac0 <YeetYeetGuac@gmail.com>
2021-12-04 09:51:44 -0500

    Split InsigniaMusic into multiple files

---
+++ addons/TAS_Insignia_Music/miscMusic.hpp
@@ -0,0 +1,115 @@
+    //Misc. Music: All Rights Reserved to the artists listed
+    class TAS_Music_1 //This is arbitrary
+	{
+		name	= "Astrid Goes For A Spin";
+		// filename, volume, pitch, duration, music class to show up in Zeus
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\AstridGoesForaSpin.ogg", db + 0, 1.0 };
+		duration=43;
+		musicClass = "TASMusicClass";
+	};
+	class TAS_Music_2
+	{
+		name	= "Awaken My Quivering Abs — Stringstorm";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\AwakenMyQuiveringAbs-Stringstorm.ogg", db + 0, 1.0 };
+		duration=201;
+		musicClass = "TASMusicClass";
+	};
+	class TAS_Music_3
+	{
+		name	= "Bit — Chris Larkin & Matt Trobbiani";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\Bit-ChrisLarkin&MattTrobbiani.ogg", db + 0, 1.0 };
+		duration=159;
+		musicClass = "TASMusicClass";
+	};
+	class TAS_Music_4
+	{
+		name	= "Dream Head — HOME";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\DreamHead-Home.ogg", db + 0, 1.0 };
+		duration=199;
+		musicClass = "TASMusicClass";
+	};
+	
+	class TAS_Music_5
+	{
+		name	= "Eye of the Storm — Watt White";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\EyeOfTheStormWattWhite.ogg", db + 0, 1.0 };
+		duration=199;
+		musicClass = "TASMusicClass";
+	};
+	class TAS_Music_6
+	{
+		name	= "Fucking Bullshit";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\FuckingBullshit.ogg", db + 0, 1.0 };
+		duration=41;
+		musicClass = "TASMusicClass";
+	};
+	class TAS_Music_7
+	{
+		name	= "Giorno's Theme (Full)";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\GiornosThemeFull.ogg", db + 0, 1.0 };
+		duration=294;
+		musicClass = "TASMusicClass";
+	};
+	class TAS_Music_8
+	{
+		name	= "Resonance — HOME";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\Resonance-Home.ogg", db + 0, 1.0 };
+		duration=212;
+		musicClass = "TASMusicClass";
+	};
+	class TAS_Music_9
+	{
+		name	= "Sabotage — Remi Gallego";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\Sabotage-RemiGallego.ogg", db + 0, 1.0 };
+		duration=247;
+		musicClass = "TASMusicClass";
+	};
+	class TAS_Music_10
+	{
+		name	= "Test Drive";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\TestDrive.ogg", db + 0, 1.0 };
+		duration=156;
+		musicClass = "TASMusicClass";
+	};
+	class TAS_Music_11
+	{
+		name	= "The Quickening — Cinematrik";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\TheQuickening-Cinematrik.ogg", db + 0, 1.0 };
+		duration=460;
+		musicClass = "TASMusicClass";
+	};
+	class TAS_Music_12
+	{
+		name	= "This Is Berk";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\ThisIsBerk.ogg", db + 0, 1.0 };
+		duration=250;
+		musicClass = "TASMusicClass";
+	};
+	class TAS_Music_13
+	{
+		name	= "Home Depot Theme Orchestra Remix";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\HomeDepotThemeOrchestralRemix.ogg", db + 0, 1.0 };
+		duration=66;
+		musicClass = "TASMusicClass";
+	};
+    class TAS_Music_14
+	{
+		name	= "Crazy Frog - Alex F (Medieval Style)";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\CrazyFrog-AxelF(MedievalStyle).ogg", db + 0, 1.0 };
+		duration=119;
+		musicClass = "TASMusicClass";
+	};
+    class TAS_Music_15
+	{
+		name	= "Kickstart My Heart - Motley Crue";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\MotleyCrue-KickstartMyHeart.ogg", db + 0, 1.0 };
+		duration=283;
+		musicClass = "TASMusicClass";
+	};
+    class TAS_Music_16
+	{
+		name	= "Kickstart My Heart [INSTRUMENTAL W/MINIMAL VOCALS]- Motley Crue";
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Insignia_Music\music\MotleyCrue-KickstartMyHeart_Instrumental.ogg", db + 0, 1.0 };
+		duration=286;
+		musicClass = "TASMusicClass";
+	};
