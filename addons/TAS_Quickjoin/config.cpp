5a48244ec180438aac7e1d8d0b2e46e2c1917a50
Guac0 <66752224+Guac0@users.noreply.github.com>
2022-02-28 18:10:19 -0500

    wip quickjoin

--- addons/TAS_Quickjoin/config.cpp
+++ addons/TAS_Quickjoin/config.cpp
@@ -24,19 +24,109 @@ class RscDisplayMain: RscStandardDisplay {
                 class ServerBrowser;
                 class JoinTasServer: ServerBrowser {
                     idc = -1;
-                    text = "TAS Server Quickjoin";
-                    tooltip = "Connect to the TAS Main Server";
+                    text = "TAS Arma Server Quickjoin";
+                    tooltip = "Connect to the Main TAS Arma Server";
                     y = "(3 * 	1.5) * 	(pixelH * pixelGrid * 2) + 	(pixelH)";
-                    onbuttonclick = "connectToServer ['82.8.46.207', 2302, '']";
+                    //onbuttonclick = "connectToServer ['d404.nl', 2302, '']"; //Cat: 82.8.46.207
+                    onbuttonclick = "0 = [_this, 'd404.nl', '2302', 'cheese'] execVM '\Autismo_Seals_Unit_Mod\addons\TAS_Quickjoin\scripts\joinServer.sqf';";
+                };
+                class JoinTasTeamspeak: ServerBrowser {
+                    idc = -1;
+                    text = "TAS Teamspeak Server Quickjoin";
+                    tooltip = "Connect to the Main TAS Teamspeak Server";
+                    y = "(4 * 	1.5) * 	(pixelH * pixelGrid * 2) + 	(pixelH)";
+                    url = "ts3server://d404.nl?port=9987&password=cheese";
+                    //onbuttonclick = "connectToServer ['71.126.163.78', 2302, '']";
                 };
                 class JoinGuacServer: ServerBrowser {
                     idc = -1;
-                    text = "TAS Primary Backup Server Quickjoin";
-                    tooltip = "Connect to the TAS Primary Backup Server";
-                    y = "(4 * 	1.5) * 	(pixelH * pixelGrid * 2) + 	(pixelH)";
-                    onbuttonclick = "connectToServer ['71.126.163.78', 2302, '']";
+                    text = "TAS Backup Arma Server Quickjoin";
+                    tooltip = "Connect to the Backup TAS Arma Server. Scroll downwards for the backup server's Teamspeak quickjoin.";
+                    y = "(5 * 	1.5) * 	(pixelH * pixelGrid * 2) + 	(pixelH)";
+                    //onbuttonclick = "connectToServer ['71.126.163.78', 2302, '']";
+                    onbuttonclick = "0 = [_this, '127.0.0.1', '2302', 'cheese'] execVM '\Autismo_Seals_Unit_Mod\addons\TAS_Quickjoin\scripts\joinServer.sqf';";
+                };
+                class JoinGuacTeamspeak: ServerBrowser {
+                    idc = -1;
+                    text = "TAS Backup Teamspeak Server Quickjoin";
+                    tooltip = "Connect to the Backup TAS Teamspeak Server";
+                    y = "(6 * 	1.5) * 	(pixelH * pixelGrid * 2) + 	(pixelH)";
+                    url = "ts3server://71.126.163.78?port=9987&password=cheese";
+                    //onbuttonclick = "connectToServer ['71.126.163.78', 2302, '']";
                 };
             };
         };
+    };
+};
+
+class CfgMainMenuSpotlight // RscDisplayMain >> Spotlight works but is considered obsolete since SPOTREP #00064
+{
+	/*class CoopCampaign
+	{
+		text = "Coop Campaign"; // Text displayed on the square button, converted to upper-case
+		textIsQuote = 1; // 1 to add quotation marks around the text
+		picture = "\a3\Ui_f\Data\GUI\Rsc\RscDisplayMain\spotlight_1_apex_ca.paa"; // Square picture, ideally 512x512
+		video = "\a3\Ui_f\Video\spotlight_1_Apex.ogv"; // Video played on mouse hover
+		action = "ctrlactivate ((ctrlparent (_this select 0)) displayctrl 101);"; // Code called upon clicking, passed arguments are [<button:Control>]
+		actionText = $STR_A3_RscDisplayMain_Spotlight_Play; // Text displayed in top left corner of on-hover white frame
+		condition = "true"; // Condition for showing the spotlight
+	};
+	class AnotherCoopCampaign
+	{
+		text = "Another Coop Campaign";
+		picture = "\a3\Ui_f\Data\GUI\Rsc\RscDisplayMain\spotlight_1_eastwind_ca.paa";
+		action = "ctrlactivate ((ctrlparent (_this select 0)) displayctrl 149);";
+		actionText = $STR_A3_RscDisplayMain_Spotlight_Play;
+		condition = "isKeyActive 'FinishedCoopCampaign';";
+	};*/
+	/*class JoinTasServer
+	{
+		text = "Join TAS Arma Server";
+		picture = "my_mod\data\spotlight.paa";
+		action = "connectToServer ['127.0.0.1', 2302, '']";
+		actionText = "Join the Splendid Server";
+		condition = "true";
+	};*/
+    class JoinTasServer {
+        text = "Connect to<br />TAS Arma Server";
+        //picture = "Autismo_Seals_Unit_Mod\logo.paa";
+        picture = "\Autismo_Seals_Unit_Mod\addons\TAS_Quickjoin\media\logo.paa";
+        action = "0 = [_this, 'd404.nl', '2302', 'cheese'] execVM '\Autismo_Seals_Unit_Mod\addons\TAS_Quickjoin\scripts\joinServer.sqf';";
+        //action = "connectToServer ['d404.nl', 2302, 'cheese']"; //Cat: 82.8.46.207
+        actionText = "Connect to the Main TAS Arma Server";
+        condition = "true";
+    };
+    class JoinTasTeamspeak {
+        text = "Connect to<br />TAS Teamspeak Server";
+        //picture = "Autismo_Seals_Unit_Mod\logo.paa";
+        picture = "\Autismo_Seals_Unit_Mod\addons\TAS_Quickjoin\media\logo.paa";
+        //action = "(_this select 1) ctrlSetURL 'http://arma3.com/';";
+        actionText = "Connect to the Main TAS Teamspeak Server";
+        url = "ts3server://d404.nl?port=9987&password=cheese";
+        condition = "true";
+        //_ctrlMenuStrip menuSetURL [[0,0,1], "https://arma3.com/"];
+        //(_this select 1) ctrlSetURL "http://arma3.com/";
+        //onbuttonclick = "connectToServer ['71.126.163.78', 2302, '']";
+    };
+    class JoinGuacServer {
+        text = "Connect to<br />TAS Backup<br />Arma Server";
+        //picture = "Autismo_Seals_Unit_Mod\logo.paa";
+        picture = "\Autismo_Seals_Unit_Mod\addons\TAS_Quickjoin\media\logo.paa";
+        //action = "connectToServer ['71.126.163.78', 2302, 'cheese']";
+        action = "0 = [_this, '127.0.0.1', '2302', 'cheese'] execVM '\Autismo_Seals_Unit_Mod\addons\TAS_Quickjoin\scripts\joinServer.sqf';";
+        actionText = "Connect to the Backup TAS Arma Server. Scroll downwards for the backup Teamspeak quickjoin.";
+        condition = "true";
+        //onbuttonclick = "connectToServer ['71.126.163.78', 2302, '']";
+    };
+    class JoinGuacTeamspeak {
+        text = "Connect to<br />TAS Backup<br />Teamspeak Server";
+        //picture = "Autismo_Seals_Unit_Mod\logo.paa";
+        picture = "\Autismo_Seals_Unit_Mod\addons\TAS_Quickjoin\media\logo.paa";
+        //action = "connectToServer ['127.0.0.1', 2302, '']";
+        actionText = "Connect to the Backup TAS Teamspeak Server";
+        //url = "ts3server://71.126.163.78?port=9987&password=cheese";
+        url = "http://arma3.com/";
+        condition = "true";
+        //onbuttonclick = "connectToServer ['71.126.163.78', 2302, '']";
     };
 };
