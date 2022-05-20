8b08e204b6508b10ed970b67a1be7f3e4d59fd98
Guac0 <66752224+Guac0@users.noreply.github.com>
2022-05-19 20:26:55 -0400

    FFA general fixes, brought up to Pizza Factions equivalency

--- addons/TAS_FFA_Factions/config.cpp
+++ addons/TAS_FFA_Factions/config.cpp
@@ -5,12 +5,19 @@ class CfgPatches
 	{
 		name = "TAS FFA Faction Addon";
 		units[] = {
-			"TAS_FFA_Sol1Unit",
-			"TAS_FFA_OffUnit"
+			//"TAS_FFA_Soldier",
+			"TAS_FFA_Officer",
+			"TAS_FFA_Headgear_Helmet",
+			"TAS_FFA_BaseUnit",
+			"TAS_FFA_Sidearm",
+			"TAS_FFA_Rifleman",
+			"TAS_FFA_TeamLeader",
+			"TAS_FFA_Operative"
 		};
 		weapons[] = {
-			"FFA_soldier1_uniform",
-			"FFA_officer_uniform"
+			"TAS_FFA_uniformSoldier",
+			"TAS_FFA_uniformOfficer",
+			"TAS_FFA_Helmet"
 		};
 		requiredAddons[] = {
 			"A3_Characters_F"
@@ -20,8 +27,55 @@ class CfgPatches
 	};
 };
 
+class CBA_Extended_EventHandlers_base; //we'll use this for the vehicles' CBA compat later (units already have compat, perhaps through inheritance that didn't work for vics?)
+
+//************************************************************************************************************************************************************************************************
+//*****             Definitions                *********************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+#define MAG_1(a) a
+#define MAG_2(a) a, a
+#define MAG_3(a) a, a, a
+#define MAG_4(a) a, a, a, a
+#define MAG_5(a) a, a, a, a, a
+#define MAG_6(a) a, a, a, a, a, a
+#define MAG_7(a) a, a, a, a, a, a, a
+#define MAG_8(a) a, a, a, a, a, a, a, a
+#define MAG_9(a) a, a, a, a, a, a, a, a, a
+#define MAG_10(a) a, a, a, a, a, a, a, a, a, a
+#define MAG_11(a) a, a, a, a, a, a, a, a, a, a, a
+#define MAG_12(a) a, a, a, a, a, a, a, a, a, a, a, a
+
+//************************************************************************************************************************************************************************************************
+//*****        Factions                  *********************************************************************************************************************************************************
+//************************************************************************************************************************************************************************************************
+class cfgFactionClasses 
+{  
+	class TAS_FFA_Faction
+    { 
+        displayName = "FFA"; 
+        priority = 1; 	// Position in list. 
+        side = 2; 		// Opfor = 0, Blufor = 1, Indep = 2. 
+        icon = ""; 		//Custom Icon 
+    };
+};
+
+//see https://community.bistudio.com/wiki/Eden_Editor:_Object_Categorization, we are interested in subcatagories which are normally "APC", "Men", etc
+//catagory is set my "faction" config parameter below
+//subcatagory is set by editorSubcategory = "MySubcategory"; in config
+class CfgEditorSubcategories
+{
+	class TAS_FFAA_SubGroup_Men // Category class, you point to it in editorSubcategory property
+	{
+		displayName = "FFA Men"; // Name visible in the list
+	};
+	/*class TAS_FFAA_SubGroup_Cars
+	{
+		displayName = "Pizza Delivery Mobiles";
+	};*/
+};
+
 #include "CfgVehicles.hpp"
 
-//#include "CfgGroups.hpp"
+#include "CfgGroups.hpp"
 
 #include "CfgWeapons.hpp"
