21eeaa934f3ea5f3da9049e3119284275b21a1d1
Guac0 <66752224+Guac0@users.noreply.github.com>
2022-05-19 20:50:53 -0400

    added MP compat to minefield sign, reverted foxhole animation change

--- addons/TAS_AceActions/config.cpp
+++ addons/TAS_AceActions/config.cpp
@@ -22,7 +22,7 @@ class CfgVehicles {
                     displayName = "Place Minefield Sign";
 					condition = "ACE_player == (vehicle ACE_player)"; //only if on foot (would also do need a defusal kit but 1. no frame-friendly way of checking and 2. everyone should be able to mark mines)
 					exceptions[] = {};
-					statement = "ACE_player playActionNow 'medic'; [6, [], {_obj = createVehicle ['Land_Sign_Mines_F', position ACE_player, [], 0, 'CAN_COLLIDE']; [_obj, true, [0, 2, 0], 0] call ace_dragging_fnc_setCarryable; [ace_player,_obj] call ace_dragging_fnc_carryObject; }, {ACE_player playActionNow 'medicstop';}, 'Unfolding sign...'] call ace_common_fnc_progressBar"; //start dragging after deployed. //ACE_player switchMove ''; //to get unstuck
+					statement = "ACE_player playActionNow 'medic'; [6, [], {_obj = createVehicle ['Land_Sign_Mines_F', position ACE_player, [], 0, 'CAN_COLLIDE']; [_obj, true, [0, 2, 0], 0] remoteExecCall ['ace_dragging_fnc_setCarryable']; [ace_player,_obj] call ace_dragging_fnc_carryObject; }, {ACE_player playActionNow 'medicstop';}, 'Unfolding sign...'] call ace_common_fnc_progressBar"; //start dragging after deployed. //ACE_player switchMove ''; //to get unstuck
                         //Small triangular sign: Land_Sign_Mines_F
                         //Tall triangular sign: Land_Sign_MinesTall_English_F
                         //Huge rectangular sign: Land_Sign_MinesDanger_English_F
@@ -39,7 +39,7 @@ class CfgVehicles {
 					displayName = "Dig Foxhole";
 					condition = "_player call ace_trenches_fnc_canDigTrench"; //let ace handle checking
 					exceptions[] = {};
-					statement = "ACE_player playActionNow 'AinvPknlMstpSnonWnonDnon_medic4'; [10, [], {_obj = createVehicle ['vurtual_foxhole', position ACE_player, [], 0, 'CAN_COLLIDE']; _obj enableSimulationGlobal true;}, {ACE_player playActionNow 'medicstop';}, 'Digging Foxhole...'] call ace_common_fnc_progressBar";
+					statement = "ACE_player playActionNow 'medic'; [10, [], {_obj = createVehicle ['vurtual_foxhole', position ACE_player, [], 0, 'CAN_COLLIDE']; _obj enableSimulationGlobal true;}, {ACE_player playActionNow 'medicstop';}, 'Digging Foxhole...'] call ace_common_fnc_progressBar";
 					//icon = ""; //might add a custom icon later, for now let's see what default looks like
 				};
 			};
