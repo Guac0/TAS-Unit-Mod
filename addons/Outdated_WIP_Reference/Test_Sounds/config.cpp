b76104adcfd1440be43627796f89ce9ced16dfc0
Guac0 <66752224+Guac0@users.noreply.github.com>
2022-06-16 20:18:58 -0400

    made release files

--- addons/Outdated_WIP_Reference/Test_Sounds/config.cpp
+++
@@ -1,7 +0,0 @@
-#include "cfgPatches.hpp"
-
-#include "cfgSoundCurves.hpp" // attenuation curves
-#include "cfgSoundShaders.hpp" // SoundShaders
-#include "cfgSoundSets.hpp" // SoundSets
-#include "cfgSound3DProcessors.hpp" // 3D processors configuration
-#include "cfgSoundFilters.hpp" // DSP filters configuration
