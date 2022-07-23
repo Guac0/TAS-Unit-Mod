da23213f44f1317bb6601d9d9c0e4246b4af8c19
Guac0 <66752224+Guac0@users.noreply.github.com>
2022-07-22 22:37:37 -0400

    added shep's music

--- addons/TAS_Community_Music/config.cpp
+++ addons/TAS_Community_Music/config.cpp
@@ -34,7 +34,17 @@ class CfgMusic
 
 	tracks[] = {
 		TAS_Guac_1,
-		TAS_Guac_2 //every music track as a comma after it except for the last music track in the list
+		TAS_Guac_2,
+		TAS_Shep_1,
+		TAS_Shep_2,
+		TAS_Shep_3,
+		TAS_Shep_4,
+		TAS_Shep_5,
+		TAS_Shep_6,
+		TAS_Shep_7,
+		TAS_Shep_8,
+		TAS_Shep_9,
+		TAS_Shep_10,//every music track as a comma after it except for the last music track in the list
 	};
 	class TAS_Guac_1
 	{
@@ -50,7 +60,76 @@ class CfgMusic
 		duration=148;
 		musicClass = "TASCommunityMusicClass";
 	};
-	
+	class TAS_Shep_1
+	{
+		name	= "Extraction Theme (In Cold Blood(Baauer Remix))";
+		duration=171;
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Community_Music\music\Shep\alt-J - In Cold Blood (Baauer Remix).ogg", db + 0, 1.0 }; 
+		musicClass = "TASCommunityMusicClass"; 
+	};
+	class TAS_Shep_2
+	{
+		name	= "Bohemian WAApsody";
+		duration=364;
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Community_Music\music\Shep\Bohemian WAApsody (Queen sung by five Waluigis).ogg", db + 0, 1.0 }; 
+		musicClass = "TASCommunityMusicClass"; 
+	};
+	class TAS_Shep_3
+	{
+		name	= "WAAfrica";
+		duration=300;
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Community_Music\music\Shep\'WAAfrica' (Five Waluigis sing Africa).ogg", db + 0, 1.0 }; 
+		musicClass = "TASCommunityMusicClass"; 
+	};
+	class TAS_Shep_4
+	{
+		name	= "Pepsiman Theme";
+		duration=245;
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Community_Music\music\Shep\James Shimoji - Pepsi Man Theme Song (ORIGINAL).ogg", db + 0, 1.0 }; 
+		musicClass = "TASCommunityMusicClass"; 
+	};
+	class TAS_Shep_5
+	{
+		name	= "Girei(Pain's Theme)";
+		duration=134;
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Community_Music\music\Shep\Naruto Shippuden - Girei (Pain's Theme Song).ogg", db + 0, 1.0 }; 
+		musicClass = "TASCommunityMusicClass"; 
+	};
+	class TAS_Shep_6
+	{
+		name	= "Reverb Fart";
+		duration=8;
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Community_Music\music\Shep\Reverb fart sound effect.ogg", db + 0, 1.0 }; 
+		musicClass = "TASCommunityMusicClass"; 
+	};
+	class TAS_Shep_7
+	{
+		name	= "Where is Your God Now - by Rok Nardin";
+		duration=156;
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Community_Music\music\Shep\Rok Nardin - Where is Your God Now.ogg", db + 0, 1.0 }; 
+		musicClass = "TASCommunityMusicClass"; 
+	};
+	class TAS_Shep_8
+	{
+		name	= "Sugaan Essena - by The HU";
+		duration=396;
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Community_Music\music\Shep\Sugaan Essena (Original Music from 'Star Wars Jedi: Fallen Order').ogg", db + 0, 1.0 }; 
+		musicClass = "TASCommunityMusicClass"; 
+	};
+	class TAS_Shep_9
+	{
+		name	= "At Butts Gate - Doom Fart";
+		duration=96;
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Community_Music\music\Shep\TOOT - F1B1 - At Butt's Gate (Doom fart Version).ogg", db + 0, 1.0 }; 
+		musicClass = "TASCommunityMusicClass"; 
+	};
+	class TAS_Shep_10
+	{
+		name	= "Break My Stride Fart Version";
+		duration=157;
+		sound[]	= { "Autismo_Seals_Unit_Mod\addons\TAS_Community_Music\music\Shep\Break My Stride Fart Full Version.ogg", db + 0, 1.0 }; 
+		musicClass = "TASCommunityMusicClass"; 
+	};
 	//Paste what you copied from Lines 26-32 on the line above this one. If there is not currently an empty line, make one. After pasting, make sure it looks like the examples, and then customize according to your song's file path, name, duration, etc
 	//When you are done pasting and editing your music's information, add the classname (the name immediantly after the "class" selection in your copy paste, like "TAS_Guac_1" in "class TAS_Guac_1") to the "tracks[]" section. Put a comma after every line except for last line
 };
