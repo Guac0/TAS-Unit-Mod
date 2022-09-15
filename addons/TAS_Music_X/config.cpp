9dcb7608e825f816e4c35269c11aac71e6a35816
Guac0 <66752224+Guac0@users.noreply.github.com>
2022-09-14 21:21:27 -0400

    WIP XCOM music addition

---
+++ addons/TAS_Music_X/config.cpp
@@ -0,0 +1,149 @@
+class CfgPatches
+{
+	class TAS_Music_X
+	{
+		name = "TAS Music Addon — Xcom Section";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {};
+		author = "TAS Mod Team";
+		authorUrl = "https://discord.gg/invite/mcXfzqR5Kn";
+	};
+};
+class CfgMusic
+{
+	tracks[] = {"TAS_XCOML_01","TAS_XCOML_02","TAS_XCOML_03","TAS_XCOML_04","TAS_XCOML_05","TAS_XCOML_06","TAS_XCOML_07","TAS_XCOML_08","TAS_XCOML_09","TAS_XCOML_10","TAS_XCOML_11","TAS_XCOML_12","TAS_XCOML_13","TAS_XCOML_14","TAS_XCOML_15","TAS_XCOML_16","TAS_XCOML_17","TAS_XCOML_18"};
+	class TAS_XCOML_01
+	{
+		name = "(1) UFO DEFENSE";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_01_UFODEFENSE.ogg","db + 0",1.0};
+		duration = 184;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_02
+	{
+		name = "(2) GEOSCAPE";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_02_GEOSCAPE.ogg","db + 0",1.0};
+		duration = 222;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_03
+	{
+		name = "(3) INTERCEPT";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_03_INTERCEPT.ogg","db + 0",1.0};
+		duration = 176;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_04
+	{
+		name = "(4) SCANNING PROTOCOL";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_04_SCANNINGPROTOCOL.ogg","db + 0",1.0};
+		duration = 168;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_05
+	{
+		name = "(5) RESISTANCE";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_05_RESISTANCE.ogg","db + 0",1.0};
+		duration = 168;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_06
+	{
+		name = "(6) SLEEPING CHRYSSALIDS";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_06_SLEEPINGCHRYSSALIDS.ogg","db + 0",1.0};
+		duration = 141;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_07
+	{
+		name = "(7) KILL IT WITH FIRE";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_07_KILLITWITHFIRE.ogg","db + 0",1.0};
+		duration = 193;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_08
+	{
+		name = "(8) BLACKSITE";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_08_BLACKSITE.ogg","db + 0",1.0};
+		duration = 133;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_09
+	{
+		name = "(9) RETALIATION";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_09_RETALIATION.ogg","db + 0",1.0};
+		duration = 166;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_10
+	{
+		name = "(10) BEWARE THE FACELESS";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_10_BEWARETHEFACELESS.ogg","db + 0",1.0};
+		duration = 129;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_11
+	{
+		name = "(11) COVERT OPERATIONS";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_11_COVERTOPERATIONS.ogg","db + 0",1.0};
+		duration = 203;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_12
+	{
+		name = "(12) LOST";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_12_LOST.ogg","db + 0",1.0};
+		duration = 120;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_13
+	{
+		name = "(13) ALIEN BASE ASSAULT";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_13_ALIENBASEASSAULT.ogg","db + 0",1.0};
+		duration = 222;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_14
+	{
+		name = "(14) DEATH APPROACHES";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_14_DEATHAPPROACHES.ogg","db + 0",1.0};
+		duration = 134;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_15
+	{
+		name = "(15) WARLOCK";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_15_WARLOCK.ogg","db + 0",1.0};
+		duration = 232;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_16
+	{
+		name = "(16) MISSION ACCOMPLISHED";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_16_MISSIONACCOMPLISHED.ogg","db + 0",1.0};
+		duration = 148;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_17
+	{
+		name = "(17) LEFT BEHIND";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_17_LEFTBEHIND.ogg","db + 0",1.0};
+		duration = 216;
+		musicClass = "TASXCOMClass";
+	};
+	class TAS_XCOML_18
+	{
+		name = "(18) MISSION ACCOMPLISHED - ADVENT BURGER REMIX";
+		sound[] = {"Autismo_Seals_Unit_Mod\addons\TAS_Music_X\music\X\XCOML_18_MISSIONACCOMPLISHED_BURGERREMIX.ogg","db + 0",1.0};
+		duration = 261;
+		musicClass = "TASXCOMClass";
+	};
+};
+class CfgMusicClasses
+{
+	class TASXCOMClass
+	{
+		displayName = "XCOM Music";
+	};
+};
