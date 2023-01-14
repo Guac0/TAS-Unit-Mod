0d01f4d5170ab7e08c0ea9cc9fb89a3ddf7f5884
Guac0 <66752224+Guac0@users.noreply.github.com>
2023-01-14 08:21:54 -0500

    adjusted stamina and sway

--- addons/TAS_ConfigEdits/config.cpp
+++ addons/TAS_ConfigEdits/config.cpp
@@ -380,12 +380,35 @@ class CfgImprecision
 	*/
 };
 
+class CfgMovesFatigue { 
+	staminaDuration = 180; //total amount of stamina. default 60
+	staminaCooldown = 5; //when you run out of stamina the sprinting is disabled for this duration. default 10
+	staminaRestoration = 15; //time required for your current stamina pool (total stamina - inventory load) to restore. default 30
+	aimPrecisionSpeedCoef = 5; //aimPrecision adjusting rate coefficient. default 5
+	terrainDrainSprint = -1; //when terrain gradient disable sprint, this stamina value is added to every animation state. default -1
+	terrainDrainRun = -1; //when terrain gradient enable force walk, this stamina value is added to every animation state. default -1
+	terrainSpeedCoef = 0.9; //when terrain gradient disable sprint, animation speed is multiplied by this value. default 0.9
+
+	/*
+	//vanilla
+	class CfgMovesFatigue
+	{
+		staminaDuration = 60;
+		staminaCooldown = 10;
+		staminaRestoration = 30;
+		aimPrecisionSpeedCoef = 5;
+		terrainDrainSprint = -1;
+		terrainDrainRun = -1;
+		terrainSpeedCoef = 0.9;
+	};
+	*/
+};
 
 class CfgBreathing
 {
-	maxHoldTime = 12;
-	coefSpeed = 2.5;
-	outOfBreathCoef = 5;
+	maxHoldTime = 15; //was 12
+	coefSpeed = 2; //was 2.5
+	outOfBreathCoef = 3; //was 5
 	/*
 	//vanilla
 	maxHoldTime = 8;
@@ -398,9 +421,9 @@ class CfgWeaponHandling
 {
 	class SwayDistortion
 	{
-		rate = 6;
-		decay = 3;
-		maximum = 4;
+		rate = 5; //was 6
+		decay = 2.5; //was 3
+		maximum = 3.5; //was 4
 		//gunnerCoef = 1.2;
 		/*
 		//vanilla
