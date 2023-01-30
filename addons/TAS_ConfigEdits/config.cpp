fc1737bd8479726fbed943eddd347c30d56941e3
Guac0 <66752224+Guac0@users.noreply.github.com>
2023-01-29 19:03:38 -0500

    give armor to rebreather

--- addons/TAS_ConfigEdits/config.cpp
+++ addons/TAS_ConfigEdits/config.cpp
@@ -158,6 +158,50 @@ class cfgWeapons {
 		class ItemInfo: ItemInfo
 		{
 			containerClass = "Supply100"; //bandalier is supply 80, carrier special rig is 100, LBV Grenadier harness is 120, carrier rig is supply 140
+			class HitpointsProtectionInfo
+			{
+				/*class Neck //rebreather doesnt have this by default
+				{
+					hitpointName = "HitNeck";
+					armor = 8;
+					passThrough = 0.5;
+				};
+				class Arms //rebreather doesnt have this by default
+				{
+					hitpointName = "HitArms";
+					armor = 8;
+					passThrough = 0.5;
+				};*/
+				class Chest //rebreather doesnt have this by default
+				{
+					hitpointName = "HitChest";
+					armor = 30; // GA carrier rig is 78
+					passThrough = 0.3; // GA carrier rig is 0.6
+				};
+				class Diaphragm
+				{
+					hitpointName = "HitDiaphragm";
+					armor = 30; //default 8, GA carrier rig is 78
+					passThrough = 0.3; //default 0.5, GA carrier rig is 0.6
+				};
+				class Abdomen
+				{
+					hitpointName = "HitAbdomen";
+					armor = 16; //default 8, GA carrier rig is 16
+					passThrough = 0.3; //default 0.5, GA carrier rig is 0.3
+				};
+				class Pelvis
+				{
+					hitpointName = "HitPelvis";
+					armor = 16; //default 8, GA carrier rig is 16
+					passThrough = 0.3; //default 0.5, GA carrier rig is 0.3
+				};
+				class Body
+				{
+					hitpointName = "HitBody";
+					passThrough = 0.3; //default 0.5, GA carrier rig is 0.6
+				};
+			};
 		};
 	};
 	
